a58b1b186ef8dd4a6a5b2636e2e1e2fc5ad60b6e
RB-Kedar <120462460+RB-Kedar@users.noreply.github.com>
2022-12-20 17:42:32 +0000

    Basic hierarchical accordians for factor/subfcator/signal editing

--- src/pages/decisionengine/CreateModel.tsx
+++ src/pages/decisionengine/CreateModel.tsx
@@ -1,18 +1,123 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import {Accordion, AccordionDetails, AccordionSummary, Box, Paper, TextField} from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 // import './styles.css';
+interface INode {
+  name:string;
+  subFactors?: INode[];
+  signals?: INode[];
+}
 
+
+const WeightEditor = ({node, ...rest} : {node:INode, [key: string]: any}) => {
+  return(
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={5} {...rest}>
+          <Grid xs={8}>
+            <Paper style={{backgroundColor:"#434DB0", color:"white", fontWeight: "bold", padding:10}}>{node.name}</Paper>
+          </Grid>
+          <Grid xs={4}>
+            <div>
+              <TextField variant="outlined" size={"small"} />
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+  )
+}
+const NodeEditor:React.FC<{ node:INode }> =  ({node}) => {
+  return (
+      <Accordion>
+        <AccordionSummary>
+          <WeightEditor node={node} />
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container>
+            <Grid xs={8} style={{paddingLeft: 50}}>
+              {node.subFactors?.map(sf => (
+                  <NodeEditor node={sf} />
+              ))}
+              {node.signals?.map(sig => (
+                  <WeightEditor node={sig} style={{marginBottom:10}} />
+              ))}
+
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+  )
+
+}
 function CreateModel() {
-  const [counter, setCounter] = React.useState(60);
-
-  React.useEffect(() => {
-    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-  }, [counter]);
+  const product = {
+    name: "Working Capital Loan",
+    factors: [
+      {
+        name: "Financial Strength",
+        subFactors: [
+          {
+            name: "Market Conditions ",
+            signals: [
+              {name: "GP%vsSector"},
+              {name: "NP%vsSector"},
+              {name: "LeveragevsSector"},
+              {name: "GearingvsSector"}
+            ]
+          },
+          {
+            name: "Debt Service",
+            signals: [
+              {name: "EBIDTA:DSC"}
+            ]
+          },
+          {
+            name: "Financial Stability",
+            signals: [
+              {name: "%ChgTurnover"},
+              {name: "EBIDTA%ratio"},
+              {name: "Stressed EBIDTA:DSC"},
+              {name: "%ChgRetainedProfits"}
+            ]
+          },
+          {
+            name: "Gearing ratio",
+            signals: [
+              {name: "Gearing"},
+            ]
+          },
+          {
+            name: "Leverage",
+            signals: [
+              {name: "Leverage"},
+            ]
+          },
+        ]
+      },
+      {
+        name: "Strength of Business Owner/Guarantor & Security Package",
+        subFactors: [
+          {
+            name: "Financial Capacity & Willingness to Support",
+            signals: [
+              {name: "Sponsors Debt"},
+              {name: "Sponsors Net Worth"},
+              {name: "Sponsor Credit Score"},
+              {name: "Business Interuption Insurance"},
+            ]
+          }
+        ]
+      }
+    ]
+  }
 
   return (
     <div className="">
       <div>create model</div>
+      {product.factors.map(f => (
+          <NodeEditor node={f} />
+      ))}
     </div>
   );
 }
