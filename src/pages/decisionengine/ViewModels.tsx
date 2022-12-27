e757da4e286ff2514635e328b479e7c2ed34ffe3
Praneeth <praneeth.rajarikam@recognisebank.co.uk>
2022-12-27 06:47:23 -0600

    Updated styles

--- src/pages/decisionengine/ViewModels.tsx
+++ src/pages/decisionengine/ViewModels.tsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel'
+import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
 
 import {
   Link,
@@ -29,56 +32,84 @@ const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
 const Paper = styled(MuiPaper)(spacing);
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 30 },
-  { field: "name", headerName: "Name", width: 150, editable: true },
+  {
+    field: "id",
+    headerName: "ID",
+    width: 10,
+    hide: true,
+    headerAlign: 'center',
+  },
+  {
+    field: "status",
+    headerName: "",
+    width: 20,
+    headerAlign: 'center',
+    renderCell: (params) => {
+      if (params.row.status == 'approved') {
+        return (<CheckCircleIcon style={{ color: 'green' }} />)
+      } else if (params.row.status == 'unapproved') {
+        return (<CancelIcon style={{ color: 'red' }} />)
+      } else {
+        return (<PendingOutlinedIcon />)
+      }
+    }
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+    description: "name",
+    headerAlign: 'center',
+  },
   {
     field: "product",
     headerName: "Product",
     width: 200,
-    editable: true,
+    description: "product",
+    headerAlign: 'center',
   },
   {
     field: "loan_range",
     headerName: "Loan Range",
     width: 150,
-    editable: true,
+    headerAlign: 'center',
   },
   {
     field: "term",
     headerName: "Term",
     width: 75,
-    editable: true,
+    headerAlign: 'center',
   },
   {
     field: "purpose",
     headerName: "Purpose",
     width: 100,
-    editable: true,
+    headerAlign: 'center',
   },
   {
     field: "secured",
     headerName: "Secured",
     width: 75,
-    editable: true,
+    headerAlign: 'center',
   },
   {
     field: "created_by",
     headerName: "Created By",
     width: 120,
-    editable: true,
+    headerAlign: 'center',
   },
 ];
 
 const rows = [
-  { id: 1, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher" },
-  { id: 2, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher" },
-  { id: 3, name: "Business line of credit", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax, Growth", secured: "yes", created_by: "Christopher" },
-  { id: 4, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher" },
-  { id: 5, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher" },
-  { id: 6, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher" },
-  { id: 7, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher" },
-  { id: 8, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "no", created_by: "Christopher" },
-  { id: 9, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth, Tax", secured: "yes", created_by: "Christopher" },
+  { id: 1, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved" },
+  { id: 2, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved" },
+  { id: 3, name: "Business line of credit", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-4 Y", purpose: "Tax, Growth", secured: "yes", created_by: "Christopher", status: "in-review" },
+  { id: 4, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved" },
+  { id: 5, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-6 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved" },
+  { id: 6, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "in-review" },
+  { id: 7, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "1-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "approved" },
+  { id: 8, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Tax", secured: "no", created_by: "Christopher", status: "unapproved" },
+  { id: 9, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth, Tax", secured: "yes", created_by: "Christopher", status: "in-review" },
 ];
 
 function DataGridDemo() {
