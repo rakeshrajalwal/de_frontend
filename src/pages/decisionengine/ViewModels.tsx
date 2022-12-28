6704039db5e0258a8fc597b97c2d7e5d8e5e9040
Praneeth <praneeth.rajarikam@recognisebank.co.uk>
2022-12-28 07:00:00 -0600

    Added is active chips

--- src/pages/decisionengine/ViewModels.tsx
+++ src/pages/decisionengine/ViewModels.tsx
@@ -1,31 +1,38 @@
 import * as React from 'react';
-import { render } from 'react-dom';
 import styled from "@emotion/styled";
-import { NavLink } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel'
 import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
 
 import {
-  Link,
   Breadcrumbs as MuiBreadcrumbs,
   Card as MuiCard,
   CardContent as MuiCardContent,
   Divider as MuiDivider,
+  Chip as MuiChip,
   Paper as MuiPaper,
   Typography,
 } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { spacing } from "@mui/system";
+import { spacing, SpacingProps } from "@mui/system";
+import createTheme from '../../theme';
 
 // import './styles.css';
+
+interface ChipProps extends SpacingProps {
+  component?: React.ElementType;
+  href?: string;
+  icon?: JSX.Element | null;
+}
 
 const Card = styled(MuiCard)(spacing);
 
 const CardContent = styled(MuiCardContent)(spacing);
 
 const Divider = styled(MuiDivider)(spacing);
+
+const Chip = styled(MuiChip)<ChipProps>(spacing);
 
 const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
 
@@ -60,6 +67,7 @@ const columns: GridColDef[] = [
     width: 150,
     description: "name",
     headerAlign: 'center',
+    
   },
   {
     field: "product",
@@ -98,29 +106,43 @@ const columns: GridColDef[] = [
     width: 120,
     headerAlign: 'center',
   },
+  {
+    field: "is_active",
+    headerName: "",
+    width: 100,
+    renderCell: (params) => {
+      if (params.row.is_active) {
+        return (<Chip label="Active" color="primary" variant="outlined" m={1} size='small' style={{ borderRadius: 10 }} />)
+      } else {
+        return (<Chip label="Inactive" variant="outlined" m={1} size='small' style={{ borderRadius: 10 }} />)
+      }
+    }
+  },
 ];
 
 const rows = [
-  { id: 1, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved" },
-  { id: 2, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved" },
-  { id: 3, name: "Business line of credit", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-4 Y", purpose: "Tax, Growth", secured: "yes", created_by: "Christopher", status: "in-review" },
-  { id: 4, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved" },
-  { id: 5, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-6 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved" },
-  { id: 6, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "in-review" },
-  { id: 7, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "1-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "approved" },
-  { id: 8, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Tax", secured: "no", created_by: "Christopher", status: "unapproved" },
-  { id: 9, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth, Tax", secured: "yes", created_by: "Christopher", status: "in-review" },
+  { id: 1, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved", is_active: true },
+  { id: 2, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved", is_active: false },
+  { id: 3, name: "Business line of credit", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-4 Y", purpose: "Tax, Growth", secured: "yes", created_by: "Christopher", status: "in-review", is_active: true },
+  { id: 4, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "unapproved", is_active: false },
+  { id: 5, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-6 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "approved", is_active: true },
+  { id: 6, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Tax", secured: "yes", created_by: "Christopher", status: "in-review", is_active: true },
+  { id: 7, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "1-5 Y", purpose: "Growth", secured: "no", created_by: "Christopher", status: "approved", is_active: false },
+  { id: 8, name: "Invoice Financing", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "2-5 Y", purpose: "Tax", secured: "no", created_by: "Christopher", status: "unapproved", is_active: true },
+  { id: 9, name: "Term Loan", product: "Working Capital Loan", loan_range: "100000 - 300000", term: "3-5 Y", purpose: "Growth, Tax", secured: "yes", created_by: "Christopher", status: "in-review", is_active: false },
 ];
 
-function DataGridDemo() {
+function ModelDataGrid() {
   return (
     <Paper>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
+          // sx={{ backgroundColor: '#D9F1FC' }}
           rowsPerPageOptions={[5, 10, 25]}
           rows={rows}
           columns={columns}
           pageSize={5}
+          style={{ fontFamily: 'Verdana', alignSelf: 'center' }}
         />
       </div>
     </Paper>
@@ -137,7 +159,7 @@ function ViewModels() {
 
       <Divider my={6} />
 
-      <DataGridDemo />
+      <ModelDataGrid />
     </React.Fragment>
   );
 }
