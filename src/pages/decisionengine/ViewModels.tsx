0bdfa35d9b1e6d6e3f22d458021e04a6b5031f8e
Praneeth <praneeth.rajarikam@recognisebank.co.uk>
2023-01-05 05:14:10 -0600

    Updated SVG logo and used models collection json to render data grid

--- src/pages/decisionengine/ViewModels.tsx
+++ src/pages/decisionengine/ViewModels.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import * as React from "react";
 import styled from "@emotion/styled";
 import { Helmet } from "react-helmet-async";
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 import {
   Chip as MuiChip,
   Paper as MuiPaper,
@@ -10,14 +10,10 @@ import {
   Typography,
   IconButton,
 } from "@mui/material";
-import {
-  DataGrid,
-  GridColDef,
-  GridToolbar,
-} from "@mui/x-data-grid";
-import { spacing, } from "@mui/system";
-import AccessTime from '@mui/icons-material/AccessTime';
-import modelsJson from './getmodels.json';
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import { spacing } from "@mui/system";
+import AccessTime from "@mui/icons-material/AccessTime";
+import modelsJson from "./getmodels.json";
 
 export interface model {
   id: number;
@@ -37,64 +33,68 @@ export interface model {
 }
 
 export interface IProduct {
-  name: string,
+  name: string;
   factors: {
-    name: string,
+    name: string;
     subFactors: {
-      name: string,
+      name: string;
       signals: {
-        name: string
-      }[]
-    }[]
-  }[]
+        name: string;
+      }[];
+    }[];
+  }[];
 }
 
-
-
 export interface IRange {
-  min: number | string,
-  max: number | string,
-  _id: string,
+  min: number | string;
+  max: number | string;
+  _id: string;
 }
 
 export interface ICriteria {
-  weak: IRange,
-  satisfactory: IRange,
-  good: IRange,
-  strong: IRange
+  weak: IRange;
+  satisfactory: IRange;
+  good: IRange;
+  strong: IRange;
 }
 
 export interface IPolicy {
-  name: string,
-  loanRange: IRange,
-  loanTermInMonths: IRange,
-  loanPurpose: string[],
-  isSecured: boolean
+  name: string;
+  loanRange: IRange;
+  loanTermInMonths: IRange;
+  loanPurpose: string[];
+  isSecured: boolean;
 }
 
 export interface IModel {
-  __v: number | string,
-  _id: string,
-  name: string,
-  product: string,
-  policy: IPolicy,
+  __v: number | string;
+  _id: string;
+  name: string;
+  product: string;
+  is_active: boolean;
+  status: string;
+  last_run_by: string;
+  last_run_on: string;
+  created_by: string;
+  created_on: string;
+  policy: IPolicy;
   factors: {
-    _id: string,
-    name: string,
-    weight: number | string,
+    _id: string;
+    name: string;
+    weight: number | string;
     subFactors: {
-      _id: string,
-      name: string,
-      weight: number | string,
+      _id: string;
+      name: string;
+      weight: number | string;
       signals: {
-        _id: string,
-        name: string,
-        weight: number | string,
-        criteria: ICriteria,
-        overallWeight: number | string,
-      }[]
-    }[]
-  }[]
+        _id: string;
+        name: string;
+        weight: number | string;
+        criteria: ICriteria;
+        overallWeight: number | string;
+      }[];
+    }[];
+  }[];
 }
 
 const Chip = styled(MuiChip)(spacing);
@@ -105,73 +105,75 @@ const Paper = styled(MuiPaper)(spacing);
 
 const paperSx = {
   "& MuiPaper-root": {
-    fontFamily: 'Verdana',
+    fontFamily: "Verdana",
   },
   "& .css-1bpvgg-MuiPaper-root": {
-    padding: '10rem',
-  }
-}
+    padding: "10rem",
+  },
+};
 
 const datagridSx = {
   "& .MuiDataGrid-columnHeaders": {
     backgroundColor: "#D9F1FC",
   },
   "& .MuiDataGrid-columnHeaderTitle": {
-    fontWeight: 'bold',
-    letterSpacing: '0.05ex',
+    fontWeight: "bold",
+    letterSpacing: "0.05ex",
     opacity: 1,
-    color: '#1B2430',
+    color: "#1B2430",
   },
   "& .MuiPaper-elevation1": {
-    backgroundColor: '#F3FBFE',
+    backgroundColor: "#F3FBFE",
   },
   "& .MuiDataGrid-cellContent": {
-    wordWrap: 'break-word !important',
-    textAlign: 'center',
-    fontSize: '1.5ex',
-    justifyContent: 'center',
-    color: '#1B2430',
-    letterSpacing: '0.05ex',
+    wordWrap: "break-word !important",
+    textAlign: "center",
+    fontSize: "1.5ex",
+    justifyContent: "center",
+    color: "#1B2430",
+    letterSpacing: "0.05ex",
     opacity: 1,
   },
   "& .MuiDataGrid-cell": {
-    whiteSpace: 'normal !important',
+    whiteSpace: "normal !important",
   },
   "& .MuiDataGrid-toolbarQuickFilter": {
-    border: 'solid',
+    border: "solid",
   },
   "& .MuiChip-label": {
-    textAlign: 'center',
-    fontSize: '1.5ex',
+    textAlign: "center",
+    fontSize: "1.5ex",
   },
   "& .MuiDataGrid-toolbarContainer": {
-    backgroundColor: '#F7F9FC',
+    backgroundColor: "#F7F9FC",
   },
 };
 
 const columns: GridColDef[] = [
   {
-    field: "id",
+    field: "_id",
     headerName: "ID",
     // width: 10,
     hide: true,
-    headerAlign: 'center',
+    headerAlign: "center",
   },
   {
     field: "status",
     headerName: "",
     flex: 0.2,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
     renderCell: (params) => {
-      if (params.row.status == 'approved') {
-        return (<CheckCircleIcon style={{ color: 'green', fontSize: '2.5ex' }} />)
-      } else if (params.row.status == 'rejected') {
-        return (<CancelIcon style={{ color: 'red', fontSize: '2.5ex' }} />)
+      if (params.row.status == "approved") {
+        return (
+          <CheckCircleIcon style={{ color: "green", fontSize: "2.5ex" }} />
+        );
+      } else if (params.row.status == "rejected") {
+        return <CancelIcon style={{ color: "red", fontSize: "2.5ex" }} />;
       } else {
-        return (<AccessTime style={{ color: 'orange', fontSize: '2.5ex' }} />)
+        return <AccessTime style={{ color: "orange", fontSize: "2.5ex" }} />;
       }
-    }
+    },
   },
   {
     field: "name",
@@ -179,8 +181,8 @@ const columns: GridColDef[] = [
     description: "Name",
     // width: 150,
     flex: 7,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
   },
   {
     field: "product",
@@ -188,44 +190,72 @@ const columns: GridColDef[] = [
     description: "Product",
     // width: 200,
     flex: 7,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
   },
   {
-    field: "loan_range",
-    headerName: "Loan Range",
-    description: "Loan Range",
+    field: "policy.loanRange",
+    headerName: "Loan Range (£)",
+    description: "Loan Range (£)",
     // width: 150,
     flex: 7,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
+    renderCell: (params) => {
+      return (<div style={{ textAlign: "center", fontSize: "1.6ex" }}>
+        {params.row.policy.loanRange.min}
+        -
+        {params.row.policy.loanRange.max}
+      </div>);
+    }
   },
   {
-    field: "term",
-    headerName: "Term",
-    description: "Term",
+    field: "policy.loanTermInMonths",
+    headerName: "Term (months)",
+    description: "Term (months)",
     // width: 75,
     flex: 4,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
+    renderCell: (params) => {
+      return (<div style={{ textAlign: "center", fontSize: "1.6ex" }}>
+        {params.row.policy.loanTermInMonths.min}
+        -
+        {params.row.policy.loanTermInMonths.max}
+      </div>);
+    }
   },
   {
-    field: "purpose",
+    field: "policy.loanPurpose",
     headerName: "Purpose",
     description: "Purpose",
     // width: 100,
     flex: 5,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
+    renderCell: (params) => {
+      return (<div style={{ textAlign: "center", fontSize: "1.6ex" }}>
+        {params.row.policy.loanPurpose.toString()}
+      </div>);
+    }
   },
   {
-    field: "secured",
+    field: "policy.isSecured",
     headerName: "Secured",
     description: "Secured",
     // width: 75,
     flex: 5,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
+    renderCell: (params) => {
+      let rendered_value = 'no';
+      if (params.row.policy.isSecured) {
+        rendered_value = 'yes';
+      }
+      return (<div style={{ textAlign: "center", fontSize: "1.6ex" }}>
+        {rendered_value}
+      </div>);
+    }
   },
   {
     field: "runs",
@@ -233,20 +263,29 @@ const columns: GridColDef[] = [
     description: "Runs",
     // width: 75,
     flex: 4,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
   },
   {
-    field: "last_run",
+    field: "last_run_by",
     headerName: "Last Run",
     description: "Last Run",
     // width: 75,
     flex: 7,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
     renderCell: (params) => {
-      return (<div style={{ textAlign: 'center', fontSize: '1.6ex' }}>{params.row.last_run}<br /><span style={{ fontSize: '1.5ex' }}>{params.row.last_run_date}</span></div>);
-    }
+      return (
+        <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', fontSize: '1.6ex' }}>
+          <div>
+            {params.row.last_run_by}
+          </div>
+          <div style={{ fontSize: '1.4ex' }}>
+            {params.row.last_run_on}
+          </div>
+        </div>
+      );
+    },
   },
   {
     field: "created_by",
@@ -254,55 +293,70 @@ const columns: GridColDef[] = [
     description: "Created By",
     // width: 120,
     flex: 7,
-    headerAlign: 'center',
-    align: 'center',
+    headerAlign: "center",
+    align: "center",
     renderCell: (params) => {
-      return (<div style={{ textAlign: 'center', fontSize: '1.6ex' }}>{params.row.created_by}<br /><span style={{ fontSize: '1.5ex' }}>{params.row.created_on} </span></div>);
-    }
+      return (
+        <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', fontSize: '1.6ex' }}>
+          <div>
+            {params.row.created_by}
+          </div>
+          <div style={{ fontSize: '1.4ex' }}>
+            {params.row.created_on}
+          </div>
+        </div>
+      );
+    },
   },
   {
     field: "is_active",
     headerName: "",
     // width: 100,
     flex: 5,
-    align: 'center',
+    align: "center",
     renderCell: (params) => {
       if (params.row.is_active) {
-        return (<Chip label="Active" color="primary" variant="outlined" m={1} size='small' style={{ borderRadius: '0.3rem', blockSize: '2.6ex' }} />)
+        return (
+          <Chip
+            label="Active"
+            color="primary"
+            variant="outlined"
+            m={1}
+            size="small"
+            style={{ borderRadius: "0.3rem", blockSize: "2.6ex" }}
+          />
+        );
       } else {
-        return (<Chip label="Inactive" variant="outlined" m={1} size='small' style={{ borderRadius: '0.3rem', blockSize: '2.6ex' }} />)
+        return (
+          <Chip
+            label="Inactive"
+            variant="outlined"
+            m={1}
+            size="small"
+            style={{ borderRadius: "0.3rem", blockSize: "2.6ex" }}
+          />
+        );
       }
-    }
+    },
   },
-];
-
-const modelRows: model[] = [
-  { id: 1, name: 'Term Loan', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '3-5 Y', purpose: 'Tax', secured: 'yes', created_by: 'Christopher', status: 'approved', is_active: true, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 2, name: 'Invoice Financing', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '2-5 Y', purpose: 'Growth', secured: 'no', created_by: 'Christopher', status: 'rejected', is_active: false, created_on: '17/12/22 12:30', last_run: '-', runs: 0, last_run_date: '' },
-  { id: 3, name: 'Business line of credit', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '2-4 Y', purpose: 'Tax, Growth', secured: 'yes', created_by: 'Christopher', status: 'in-review', is_active: true, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 4, name: 'Term Loan', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '2-5 Y', purpose: 'Growth', secured: 'no', created_by: 'Christopher', status: 'rejected', is_active: false, created_on: '11/12/22 12:30', last_run: '-', runs: 0, last_run_date: '' },
-  { id: 5, name: 'Term Loan', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '2-6 Y', purpose: 'Tax', secured: 'yes', created_by: 'Christopher', status: 'approved', is_active: true, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 6, name: 'Invoice Financing', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '3-5 Y', purpose: 'Tax', secured: 'yes', created_by: 'Christopher', status: 'in-review', is_active: true, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 7, name: 'Invoice Financing', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '1-5 Y', purpose: 'Growth', secured: 'no', created_by: 'Christopher', status: 'approved', is_active: false, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 8, name: 'Invoice Financing', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '2-5 Y', purpose: 'Tax', secured: 'no', created_by: 'Christopher', status: 'rejected', is_active: true, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
-  { id: 9, name: 'Term Loan', product: 'Working Capital Loan', loan_range: '100000 - 300000', term: '3-5 Y', purpose: 'Growth, Tax', secured: 'yes', created_by: 'Christopher', status: 'in-review', is_active: false, created_on: '11/12/22 12:30', last_run: 'ncino', runs: 634, last_run_date: '15/12/22 11:45' },
 ];
 
 function ModelDataGrid() {
   return (
     <Paper sx={paperSx}>
-      <div style={{ height: "25.2rem", width: "100%" }}>
+      <div style={{ height: '25.2rem', width: '100%' }}>
         <DataGrid
           sx={datagridSx}
           // rowsPerPageOptions={[5, 10, 25]}
-          rows={modelRows}
+          rows={modelsJson}
           columns={columns}
           pageSize={5}
+          getRowId={(row) => row._id}
           disableColumnFilter
           disableColumnSelector
           disableDensitySelector
           hideFooterSelectedRowCount
-          components={{ Toolbar: GridToolbar, }}
+          components={{ Toolbar: GridToolbar }}
           componentsProps={{
             toolbar: {
               csvOptions: { disableToolbarButton: true },
@@ -320,35 +374,59 @@ function ModelDataGrid() {
 function ViewModels() {
   return (
     <React.Fragment>
-      <Helmet title="Models" />
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', fontSize: '1.4ex' }}>
+      <Helmet title='Models' />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          fontSize: '1.4ex',
+        }}
+      >
         <div>
-          <Typography variant="h3" gutterBottom display="inline">
+          <Typography variant='h3' gutterBottom display='inline'>
             Models
           </Typography>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'right' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'right',
+          }}
+        >
           <div style={{ display: 'flex', justifyContent: 'right' }}>
-            <Button mr={1} variant="contained" color="primary" style={{ backgroundColor: '#434DB0' }}>
+            <Button
+              mr={1}
+              variant='contained'
+              color='primary'
+              style={{ backgroundColor: '#434DB0' }}
+            >
               Create Model
             </Button>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left' }}>
-            <IconButton aria-label="Approved" size="small">
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'left',
+            }}
+          >
+            <IconButton aria-label='Approved' size='small'>
               <CheckCircleIcon style={{ color: 'green', fontSize: '1.8ex' }} />
             </IconButton>
             <p>Approved </p>
 
-            <IconButton aria-label="Rejected" size="small">
+            <IconButton aria-label='Rejected' size='small'>
               <CancelIcon style={{ color: 'red', fontSize: '1.8ex' }} />
             </IconButton>
             <p>Rejected </p>
 
-            <IconButton aria-label="In-Review" size="small">
+            <IconButton aria-label='In-Review' size='small'>
               <AccessTime style={{ color: 'orange', fontSize: '1.8ex' }} />
             </IconButton>
             <p>In-Review </p>
-
           </div>
         </div>
       </div>
