de51799b8235c72219c06f2985a18aa7990ebc53
Praneeth <praneeth.rajarikam@recognisebank.co.uk>
2023-01-10 00:24:37 -0600

    Added preview model temp route

--- src/pages/decisionengine/PreviewModel.tsx
+++ src/pages/decisionengine/PreviewModel.tsx
@@ -2,7 +2,8 @@ import {
     CardContent,
     Card,
     Typography,
-    Button as MuiButton
+    Button as MuiButton,
+    Paper as MuiPaper,
 } from "@mui/material";
 import { Form, Formik } from "formik";
 import { PolicyEditor } from './components/Policy';
@@ -11,7 +12,13 @@ import { IProduct, IModel } from "./interfaces/ModelInterface";
 import './CreateModel.css';
 import styled from "@emotion/styled";
 import { spacing } from "@mui/system";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import modelsJson from "./getmodels.json";
 
+const Paper = styled(MuiPaper)(spacing);
+const Button = styled(MuiButton)(spacing);
+
+const oneModel = modelsJson[0];
 
 const Label = styled(Typography)`
     font-weight: bold;
@@ -23,7 +30,133 @@ const Range = {
     max: ''
 }
 
-const Button = styled(MuiButton)(spacing);
+const paperSx = {
+    "& MuiPaper-root": {
+        fontFamily: "Verdana",
+    },
+    "& .css-1bpvgg-MuiPaper-root": {
+        padding: "10rem",
+    },
+};
+
+const datagridSx = {
+    "& .MuiDataGrid-columnHeaders": {
+        backgroundColor: "#D9F1FC",
+    },
+    "& .MuiDataGrid-columnHeaderTitle": {
+        fontFamily: "Verdana",
+        fontWeight: "bold",
+        letterSpacing: "0.05ex",
+        opacity: 1,
+        color: "#1B2430",
+    },
+    "& .MuiPaper-elevation1": {
+        backgroundColor: "#F3FBFE",
+    },
+    "& .MuiDataGrid-cellContent": {
+        fontFamily: "Verdana",
+        wordWrap: "break-word !important",
+        textAlign: "center",
+        fontSize: "1.5ex",
+        justifyContent: "center",
+        color: "#1B2430",
+        letterSpacing: "0.05ex",
+        opacity: 1,
+    },
+    "& .MuiDataGrid-cell": {
+        whiteSpace: "normal !important",
+    },
+    "& .MuiDataGrid-toolbarQuickFilter": {
+        border: "solid",
+    },
+    "& .MuiChip-label": {
+        textAlign: "center",
+        fontSize: "1.5ex",
+    },
+    "& .MuiDataGrid-toolbarContainer": {
+        backgroundColor: "#F7F9FC",
+    },
+};
+
+const columns: GridColDef[] = [
+    {
+        field: "factors.subFactors.signals._id",
+        headerName: "ID",
+        // width: 10,
+        hide: true,
+        headerAlign: "center",
+    },
+    {
+        field: "factors.name",
+        headerName: "Factor",
+        flex: 5,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.weight",
+        headerName: "Weight",
+        description: "Factor weight",
+        // width: 150,
+        flex: 7,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.name",
+        headerName: "Sub-Factor",
+        description: "Sub-Factor",
+        // width: 200,
+        flex: 7,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.weight",
+        headerName: "Weight",
+        description: "Weight",
+        // width: 150,
+        flex: 7,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.signals.name",
+        headerName: "Signal",
+        description: "Signal",
+        // width: 75,
+        flex: 4,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.signals.weight",
+        headerName: "Weight",
+        description: "Weight",
+        // width: 100,
+        flex: 6,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.signals.overallWeight",
+        headerName: "Total weights",
+        description: "Total weights",
+        // width: 75,
+        flex: 5,
+        headerAlign: "center",
+        align: "center",
+    },
+    {
+        field: "factors.subFactors.signals.criteria",
+        headerName: "Criteria",
+        description: "Criteria",
+        // width: 75,
+        flex: 4,
+        headerAlign: "center",
+        align: "center",
+    },
+];
 
 const product: IProduct = {
     name: "Working Capital Loan",
@@ -99,14 +232,6 @@ const product: IProduct = {
     ]
 };
 
-const ControlContainer = styled.div`
-display: flex;
-gap:15px;
-align-items:baseline;
-padding-left:5px;
-padding-right:15px;
-`;
-
 function getEmptyModel(p: IProduct): IModel {
     return {
         name: '',//modelname
@@ -138,7 +263,37 @@ function getEmptyModel(p: IProduct): IModel {
     }
 }
 
-function CreateModel() {
+function ModelDataGrid() {
+    return (
+        <Paper sx={paperSx}>
+            <div style={{ height: '25.2rem', width: '100%' }}>
+                <DataGrid
+                    sx={datagridSx}
+                    // rowsPerPageOptions={[5, 10, 25]}
+                    rows={oneModel}
+                    columns={columns}
+                    pageSize={5}
+                    getRowId={(row) => row.factors.subFactors.signals._id}
+                    disableColumnFilter
+                    disableColumnSelector
+                    disableDensitySelector
+                    hideFooterSelectedRowCount
+                    components={{ Toolbar: GridToolbar }}
+                    componentsProps={{
+                        toolbar: {
+                            csvOptions: { disableToolbarButton: true },
+                            printOptions: { disableToolbarButton: true },
+                            showQuickFilter: true,
+                            quickFilterProps: { debounceMs: 500 },
+                        },
+                    }}
+                />
+            </div>
+        </Paper>
+    );
+}
+
+function PreviewModel() {
     return (
         <Formik
             initialValues={getEmptyModel(product)}
@@ -158,13 +313,6 @@ function CreateModel() {
 
                         <PolicyEditor />
 
-                        <Card sx={{ boxShadow: '0px 3px 6px #00000029', marginTop: '8px' }}>
-                            <CardContent>
-                                {formik.values.factors.map((f, i) => (
-                                    <NodeEditor key={i} node={f} path={`factors[${i}]`} level={1} />
-                                ))}
-                            </CardContent>
-                        </Card>
                     </Form>
                 );
             }}
@@ -172,4 +320,4 @@ function CreateModel() {
     )
 }
 
-export default CreateModel
+export default PreviewModel;
--- src/routes.tsx
+++ src/routes.tsx
@@ -39,6 +39,7 @@ import Landing from "./pages/presentation/Landing";
 // Protected routes
 import ProtectedPage from "./pages/protected/ProtectedPage";
 import Page404 from "./pages/auth/Page404";
+import PreviewModel from "./pages/decisionengine/PreviewModel";
 
 //const SaaS = async(() => import("./pages/dashboards/SaaS"));
 
@@ -98,6 +99,10 @@ const routes = [
       {
         path: "/model/view",
         element: <ViewModels />,
+      },
+      {
+        path: "/model/preview",
+        element: <PreviewModel />,
       },
       {
         path: "/model/run",
