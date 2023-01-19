9ef72da7e10e3e9643b0e8480781e5bbb41312ed
RB-Koushik <sai.koushik@recognisebank.co.uk>
2023-01-19 13:30:04 +0000

    merge conflicts

--- src/pages/decisionengine/interfaces/ModelInterface.tsx
+++ src/pages/decisionengine/interfaces/ModelInterface.tsx
@@ -7,7 +7,8 @@ export interface IProduct {
         subFactors: {
             name: string,
             signals: {
-                name: string
+                name: string,
+                isReverseScale?: boolean
             }[]
         }[]
     }[]
